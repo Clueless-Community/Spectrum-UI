5150a4bd6c8912bc7df2a381e6d213315069de2d
Sagar Kumar <sagarkumar.dev2301@gmail.com>
2022-10-17 18:38:14 +0530

    Refractored ui_collection

--- lib/screens/ui_collection.dart
+++ lib/screens/ui_collection.dart
@@ -4,9 +4,10 @@ import 'package:flutter_ui/UI_Pages/loginScreen_AnukirtiYadav/screens/login_scre
 import 'package:flutter_ui/UI_Pages/loginUI_Aaliya-Ali/screens/login_screen.dart';
 import 'package:flutter_ui/UI_Pages/onboardingUiPage_sagardev2301/onboarding_screen.dart';
 import 'package:flutter_ui/UI_Pages/signUpScreen_AnukirtiYadav/screens/signUp_screen.dart';
-import 'package:flutter_ui/buttons_sagardev2301/buttons_screen.dart';
-import '../UI_Pages/toggleButton_ismailyegnr/screens/toggleButton_screen.dart';
-import '../dialog_box_sagardev2301/scrollable_dialog/dialog_box_screen.dart';
+import 'package:flutter_ui/UI_Pages/buttons_sagardev2301/buttons_screen.dart';
+import 'package:flutter_ui/UI_Pages/dialog_box_sagardev2301/scrollable_dialog/dialog_box_screen.dart';
+import 'package:flutter_ui/UI_Pages/toggleButton_ismailyegnr/screens/toggle_button_screen.dart';
+
 import '../UI_Pages/upperTabbar_pratikagr/screens/upper_tab_bar.dart';
 import '../widgets/heading.dart';
 import '../widgets/screen_display.dart';
