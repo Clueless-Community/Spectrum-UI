6241c2e5c153266c31a9af48260bb61e755e3905
chandansgowda <chandansuresh007@gmail.com>
2022-12-05 21:16:23 +0530

    improved code quality

--- lib/UI_Pages/profile_screen_chandansgowda/screens/profile_screen2.dart
+++ lib/UI_Pages/profile_screen_chandansgowda/screens/profile_screen2.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../widgets/profile_attribute_button.dart';
+
 class ProfileScreen2 extends StatefulWidget {
   const ProfileScreen2({Key? key}) : super(key: key);
 
@@ -9,7 +11,6 @@ class ProfileScreen2 extends StatefulWidget {
 }
 
 class _ProfileScreen2State extends State<ProfileScreen2> {
-
   bool _isDark = true;
 
   @override
@@ -21,15 +22,28 @@ class _ProfileScreen2State extends State<ProfileScreen2> {
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          leading: IconButton(icon: Icon(Icons.arrow_back, color: _isDark ? Colors.white : Colors.black,),onPressed: (){
-            Navigator.of(context).pop();
-          },),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: _isDark ? Colors.white : Colors.black,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
           actions: [
-            IconButton(onPressed: () {
-              setState((){
-                _isDark = !_isDark;
-              });
-            }, icon: Icon(_isDark ? Icons.light_mode_outlined : Icons.dark_mode_outlined, color: _isDark ? Colors.white : Colors.black,))
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isDark = !_isDark;
+                  });
+                },
+                icon: Icon(
+                  _isDark
+                      ? Icons.light_mode_outlined
+                      : Icons.dark_mode_outlined,
+                  color: _isDark ? Colors.white : Colors.black,
+                ))
           ],
         ),
         body: Padding(
@@ -41,7 +55,7 @@ class _ProfileScreen2State extends State<ProfileScreen2> {
                 height: MediaQuery.of(context).size.height / 6,
               ),
               Center(
-                child: Stack(children: [
+                child: Stack(children: const [
                   CircleAvatar(
                     backgroundImage: NetworkImage(
                         "https://avatars.githubusercontent.com/u/41890434?v=4"),
@@ -50,30 +64,57 @@ class _ProfileScreen2State extends State<ProfileScreen2> {
                   Positioned(
                     bottom: 0,
                     right: 4,
-                    child: CircleAvatar(child: Icon(Icons.edit, color: Colors.white,),backgroundColor: Colors.blue,),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      child: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    ),
                   )
                 ]),
               ),
-              Text("Chandan Gowda", style: GoogleFonts.poppins(fontSize: 30),),
-              Text("chandan@example.com", style: GoogleFonts.poppins(fontSize: 15),),
-              SizedBox(height: MediaQuery.of(context).size.height/30,),
-              ElevatedButton(onPressed: (){}, child: Text("Upgrade to PRO", style: GoogleFonts.poppins(),), style: ElevatedButton.styleFrom(
-                shape: StadiumBorder()
-              ),),
-              SizedBox(height: 20,),
+              Text(
+                "Chandan Gowda",
+                style: GoogleFonts.poppins(fontSize: 30),
+              ),
+              Text(
+                "chandan@example.com",
+                style: GoogleFonts.poppins(fontSize: 15),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                child: Text(
+                  "Upgrade to PRO",
+                  style: GoogleFonts.poppins(),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                ProfileAttributeButton("Rating", "4.8"),
-                VerticalDivider(),
-                ProfileAttributeButton("Following", "54"),
-                VerticalDivider(),
-                ProfileAttributeButton("Followers", "2M"),
-              ],),
-              SizedBox(height: MediaQuery.of(context).size.height/30,),
-              Text("Actively looking for Summer Internships | Youtuber 25k+ | Flutter | Python | Django | "
-                  "CyberSec Lead @gdsc-jssstu | JPMC Code for Good'22 Winner",
-              style: GoogleFonts.poppins(color: Colors.grey,fontSize: 15),textAlign: TextAlign.center,)
+                children: const [
+                  ProfileAttributeButton("Rating", "4.8"),
+                  VerticalDivider(),
+                  ProfileAttributeButton("Following", "54"),
+                  VerticalDivider(),
+                  ProfileAttributeButton("Followers", "2M"),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 30,
+              ),
+              Text(
+                "Actively looking for Summer Internships | Youtuber 25k+ | Flutter | Python | Django | "
+                "CyberSec Lead @gdsc-jssstu | JPMC Code for Good'22 Winner",
+                style: GoogleFonts.poppins(color: Colors.grey, fontSize: 15),
+                textAlign: TextAlign.center,
+              )
             ],
           ),
         ),
@@ -82,21 +123,3 @@ class _ProfileScreen2State extends State<ProfileScreen2> {
   }
 }
 
-class ProfileAttributeButton extends StatelessWidget {
-  final String title, value;
-  ProfileAttributeButton(this.title,this.value);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      padding: EdgeInsets.symmetric(vertical: 5,horizontal: 10),
-      onPressed: () { },
-      child: Column(
-        children: [
-          Text(value, style: GoogleFonts.poppins(fontSize: 35),),
-          Text(title, style: GoogleFonts.poppins(fontSize: 15),)
-        ],
-      ),
-    );
-  }
-}
