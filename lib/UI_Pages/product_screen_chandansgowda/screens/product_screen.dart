805e27948c201926a975698d63eac1d898b3623b
chandansgowda <chandansuresh007@gmail.com>
2022-12-07 18:37:53 +0530

    improved responsiveness and code quality

--- lib/UI_Pages/product_screen_chandansgowda/screens/product_screen.dart
+++ lib/UI_Pages/product_screen_chandansgowda/screens/product_screen.dart
@@ -19,7 +19,7 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
           Container(
             width: double.infinity,
             height: MediaQuery.of(context).size.height * 0.6,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 color: Colors.black,
                 image: DecorationImage(
                     image: AssetImage(
@@ -35,7 +35,7 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
               backgroundColor: Colors.white,
               child: IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.arrow_back,size: 25,color: Colors.red,),
+                icon: const Icon(Icons.arrow_back,size: 25,color: Colors.red,),
                 focusColor: Colors.black,
               ),
             ),
@@ -48,7 +48,7 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
               backgroundColor: Colors.white,
               child: IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.shopping_cart_outlined,size: 25,color: Colors.red,),
+                icon: const Icon(Icons.shopping_cart_outlined,size: 25,color: Colors.red,),
                 focusColor: Colors.black,
               ),
             ),
@@ -60,14 +60,14 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
               height: MediaQuery.of(context).size.height * 0.6,
               decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
+                  borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(40),
                     topRight: Radius.circular(40),
                   ),
                   boxShadow: [
                     BoxShadow(
                         color: Colors.black.withOpacity(.2),
-                        offset: Offset(0, -4),
+                        offset: const Offset(0, -4),
                         blurRadius: 8)
                   ]),
               child: Padding(
@@ -83,24 +83,24 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
                     Text(
                       "INR 150 per Kg",
                       style: GoogleFonts.openSans(
-                          fontSize: 25, fontWeight: FontWeight.bold),
+                          fontSize: 30, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                     Container(
-                      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.redAccent, Colors.red]),
+                          borderRadius: BorderRadius.all(Radius.circular(25))),
                       child: Text(
                         "Shipped directly from farmers",
                         style: GoogleFonts.cormorant(
                             color: Colors.white, fontSize: 20),
                       ),
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: [Colors.redAccent, Colors.red]),
-                          borderRadius: BorderRadius.all(Radius.circular(25))),
                     ),
-                    Spacer(),
+                    const Spacer(),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           vertical: 10, horizontal: 50),
@@ -109,13 +109,14 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
                         children: [
                           FloatingActionButton(
                             onPressed: () {
-                              if (_quantity > 1)
+                              if (_quantity > 1) {
                                 setState(() {
                                   --_quantity;
                                 });
+                              }
                             },
-                            child: Icon(Icons.add),
                             backgroundColor: Colors.white54,
+                            child: const Icon(Icons.add),
                           ),
                           Text(
                             "$_quantity",
@@ -127,45 +128,48 @@ class _SingleProductScreenState extends State<SingleProductScreen> {
                                 ++_quantity;
                               });
                             },
-                            child: Icon(Icons.add),
                             backgroundColor: Colors.redAccent,
+                            child: const Icon(Icons.add),
                           ),
                         ],
                       ),
                     ),
-                    Spacer(),
-                    Divider(
-                      color: Colors.black,
-                      thickness: 1,
+                    const Spacer(),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Total",
-                              style: GoogleFonts.openSans(fontSize: 25),
-                            ),
-                            Text(
-                              "INR ${_quantity * 150}",
-                              style: GoogleFonts.openSans(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height*0.07,
-                          width: MediaQuery.of(context).size.width*0.3,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: FittedBox(fit: BoxFit.scaleDown,child: Text("Add To Cart",maxLines:1,style: GoogleFonts.openSans(),)),
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.red,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Total",
+                                style: GoogleFonts.openSans(fontSize: 25),
+                              ),
+                              Text(
+                                "INR ${_quantity * 150}",
+                                style: GoogleFonts.openSans(
+                                    fontSize: 25, fontWeight: FontWeight.bold),
+                              )
+                            ],
                           ),
-                        )
-                      ],
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height*0.07,
+                            width: MediaQuery.of(context).size.width*0.3,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red,),
+                              child: FittedBox(fit: BoxFit.scaleDown,child: Text("Add To Cart",maxLines:1,style: GoogleFonts.openSans(fontWeight: FontWeight.bold),)),
+                            ),
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
