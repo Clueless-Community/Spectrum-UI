abf788351f702de58e7f9e2f03b81eee704f83e7
Bhavik Kothari <kotharibhavik23@gmail.com>
2023-02-03 22:01:11 +0530

    initial push

---
+++ android/app/src/main/kotlin/com/example/flutter_component_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_component_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
