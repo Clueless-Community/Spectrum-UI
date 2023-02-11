888a70f5ae290922a269157be7c2a7b97772cf55
superiorsd10 <sachindapkara6@gmail.com>
2023-02-11 18:40:56 +0530

    adding the avatars folder

--- lib/data/widget_category.dart
+++ lib/data/widget_category.dart
@@ -1,9 +1,15 @@
+import 'package:flutter_component_ui/ui_components/avatars/avatar.dart';
+
 import '../ui_components/alerts/alert.dart';
 
 final List<Map<String, dynamic>> widgetCategoryData = [
   {
     'categoryName': 'Alerts',
     'categoryScreen': AlertScreen(),
+  },
+  {
+    'categoryName': 'Avatars',
+    'categoryScreen': AvatarScreen(),
   },
   {
     'categoryName': 'Buttons',
