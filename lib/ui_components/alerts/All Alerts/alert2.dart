b8e8839bdf5401e8f3c3e0550b05cc8dfcd3eb70
Akash srivastava <akashuna3@gmail.com>
2023-02-12 20:05:25 +0530

    alert2

--- lib/ui_components/alerts/All Alerts/alert2.dart
+++ lib/ui_components/alerts/All Alerts/alert2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Alert2 extends StatelessWidget {
   const Alert2({super.key});
@@ -6,10 +7,51 @@ class Alert2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      height: 68,
       width: double.infinity,
-      decoration: const BoxDecoration(color: Color.fromARGB(255, 73, 173, 231)),
-      child: const Center(child: Text("Alert 2")),
+      decoration: BoxDecoration(
+          color: const Color.fromRGBO(253, 192, 78, 1),
+          borderRadius: BorderRadius.circular(8)),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.info,
+            color: Colors.black,
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Message',
+                  style: TextStyle(
+                      fontFamily:
+                          GoogleFonts.publicSans(fontWeight: FontWeight.w600)
+                              .fontFamily,
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+                Text('Description',
+                    style: TextStyle(
+                        fontFamily:
+                            GoogleFonts.publicSans(fontWeight: FontWeight.w400)
+                                .fontFamily,
+                        fontSize: 18,
+                        color: Colors.black)),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.cancel_sharp,
+            color: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
