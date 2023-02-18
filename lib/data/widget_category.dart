6b46762c94f0960349972150a98563eef63e14be
Vandra Pritesh Mehulbhai <vandrapritesh2745@gmail.com>
2023-02-18 21:49:01 +0530

    upto button 6 and changes of previous pr

--- lib/data/widget_category.dart
+++ lib/data/widget_category.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_component_ui/ui_components/avatars/avatar.dart';
 import '../ui_components/alerts/alert.dart';
+import '../ui_components/buttons/buttons.dart';
+import '../ui_components/input_fields/input_field.dart';
 
 final List<Map<String, dynamic>> widgetCategoryData = [
   {
