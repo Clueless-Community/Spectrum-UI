cc9c42c11cd6ab1cb4cd13945942caa9b7751fa8
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-05-20 23:21:43 +0530

    enhanced splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,37 +12,35 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.all(30),
-          color: const Color.fromRGBO(54, 64, 183, 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image(
-                image: const AssetImage('assets/splash.png'),
-                height: size.height * 0.55,
-              ),
-              Text(
-                'Spectrum UI',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: GoogleFonts.dmSans(
-                      fontWeight: FontWeight.w700,
-                    ).fontFamily,
-                    fontSize: 36),
-              ),
-              Text(
-                'A Plug and play UI component library',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: GoogleFonts.dmSans(fontWeight: FontWeight.w400)
-                        .fontFamily,
-                    fontSize: 16),
-              ),
-            ],
-          ),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        color: const Color.fromRGBO(54, 64, 183, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(
+              image: const AssetImage('assets/splash.png'),
+              width: size.width,
+            ),
+            Text(
+              'Spectrum UI',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: GoogleFonts.dmSans(
+                    fontWeight: FontWeight.w700,
+                  ).fontFamily,
+                  fontSize: 36),
+            ),
+            Text(
+              'A Plug and play UI component library',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: GoogleFonts.dmSans(fontWeight: FontWeight.w400)
+                      .fontFamily,
+                  fontSize: 16),
+            ),
+          ],
         ),
       ),
     );
