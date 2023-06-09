3ab8b6265e05e9b93b8c786e2588535349c6e36a
Bhavani Saranga <117339714+BhavaniSaranga12@users.noreply.github.com>
2023-06-09 21:12:45 +0530

    Update fav_page.dart

--- lib/screens/main_app_widgets/fav_page.dart
+++ lib/screens/main_app_widgets/fav_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_component_ui/provider/favorite_provider.dart';
-import 'package:flutter_component_ui/theme/theme.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
